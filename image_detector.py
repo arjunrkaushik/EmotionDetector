35dc1e999390a64eeedaf5138b843537e74aa860
arjunrkaushik <62321491+arjunrkaushik@users.noreply.github.com>
2021-04-25 14:20:06 +0530

    Update image_detector.py

--- image_detector.py
+++ image_detector.py
@@ -15,7 +15,6 @@ cap = cv2.VideoCapture(0)
 
 
 while True:
-    # Grab a single frame of video
     ret, frame = cap.read()
     labels = []
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
@@ -25,31 +24,27 @@ while True:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
         roi_gray = gray[y:y+h,x:x+w]
         roi_gray = cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)
-    # rect,face,image = face_detector(frame)
-
 
         if np.sum([roi_gray])!=0:
             roi = roi_gray.astype('float')/255.0
             roi = img_to_array(roi)
             roi = np.expand_dims(roi,axis=0)
-
-        # make a prediction on the ROI, then lookup the class
-
+            
             preds = classifier.predict(roi)[0]
             label=class_labels[preds.argmax()]
             label_position = (x,y)
             cv2.putText(frame,label,label_position,cv2.FONT_HERSHEY_SIMPLEX,2,(0,255,0),3)
         else:
             cv2.putText(frame,'No Face Found',(20,60),cv2.FONT_HERSHEY_SIMPLEX,2,(0,255,0),3)
+            
+            
     cv2.imshow('Emotion Detector',frame)
     k = cv2.waitKey(1)
     if k == 27:         # wait for ESC key to exit
         cap.release()
         cv2.destroyAllWindows()
     elif k == ord('s'): # wait for 's' key to save and exit
-        cv2.imwrite(r'C:\Users\ARJUN\Expression Detector\Neutral.jpg',frame)
+        cv2.imwrite(r'C:\Users\ARJUN\Expression Detector\Capture.jpg',frame)
         cap.release()
         cv2.destroyAllWindows()
 
-#cap.release()
-#cv2.destroyAllWindows()
